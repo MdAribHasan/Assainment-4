919c2e6b54b54f4e081edc292ad7630ed869abd4
MdAribHasan <mdaribhasan00@gmail.com>
2025-02-17 23:43:27 +0600

    first commit

---
+++ one.js
@@ -0,0 +1,27 @@
+const amount = 500
+
+function calculateVAT(amount) {
+    if (typeof amount !== 'number' || amount <= 0) {
+      return "Invalid";
+    }
+    const vatAmount = amount * 7.5/100;
+    return vatAmount;
+  }
+
+
+
+  const vatHobe = calculateVAT(amount)
+  console.log('Laga joy bangla vat :',vatHobe);
+
+
+
+
+
+
+
+
+
+ 
+function  validContact( contact ) {
+  
+}
---
+++ two.js
@@ -0,0 +1,15 @@
+const contact = true;
+
+function  validContact( contact ) {
+    if(typeof contact !== 'string'){
+        return "invalid"
+    }
+     if( contact.length === 11 && contact.startsWith("01") && !contact.includes(' ')){
+        return true;
+    }
+        return false;
+    }
+
+
+const valid = validContact(contact)
+console.log(valid);
